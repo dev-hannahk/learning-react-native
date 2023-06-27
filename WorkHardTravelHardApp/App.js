9e18bac84aaf75e73efe624101ed8b80cd9e224a
Heejin Kim <dev.hannahk@gmail.com>
2023-06-27 15:24:56 +0900

    Touchables

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        {/* <TouchableOpacity activeOpacity={0.5}>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableHighlight
+          underlayColor="red"
+          activeOpacity={0.5}
+          onPress={() => console.log("pressed")}
+        >
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableHighlight> */}
+        {/* <TouchableWithoutFeedback>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => console.log("pressed?")}>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableWithoutFeedback> */}
+        <Pressable
+          hitSlop={{ bottom: 20, left: 20, right: 20, top: 20 }}
+          onPress={() => console.log("pressed!")}
+        >
+          <Text style={styles.btnText}>Work</Text>
+        </Pressable>
+        <Pressable onPress={() => console.log("pressed!")}>
+          <Text style={styles.btnText}>Travel</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +48,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 38,
   },
 });

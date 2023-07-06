cc1c3d9cf60a2527aaa81d304d83c3dcf5f8b4ca
Heejin Kim <dev.hannahk@gmail.com>
2023-07-06 17:28:52 +0900

    init react navigation

---
+++ LearnReactNavigation/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+function App(): JSX.Element {
+  return <NavigationContainer>{/* 내비게이션 설정 */}</NavigationContainer>;
+}
+
+export default App;

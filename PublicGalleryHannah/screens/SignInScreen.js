bb4f82e8ef1795ca69cb8db770cdd49abcf576cb
dev-hannahk <dev.hannahk@gmail.com>
2023-07-08 21:28:21 +0900

    user profile w/ firestore

--- PublicGalleryHannah/screens/SignInScreen.js
+++ PublicGalleryHannah/screens/SignInScreen.js
@@ -12,6 +12,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import SignButtons from '../components/SignButtons';
 import SignForm from '../components/SignForm';
 import {signIn, signUp} from '../lib/auth';
+import {getUser} from '../lib/users';
 
 function SignInScreen({navigation, route}) {
   const {isSignUp} = route.params ?? {};
@@ -43,7 +44,12 @@ function SignInScreen({navigation, route}) {
 
     try {
       const {user} = isSignUp ? await signUp(info) : await signIn(info);
-      console.log(user);
+      const profile = await getUser(user.uid);
+
+      if (!profile) {
+        navigation.navigate('Welcome', {uid: user.uid});
+      } else {
+      }
     } catch (e) {
       const messages = {
         'auth/email-already-in-use': '이미 가입된 이메일입니다.',

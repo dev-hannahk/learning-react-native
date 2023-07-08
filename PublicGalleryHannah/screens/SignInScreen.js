7efc4233648025206bbc9c046c6391cc4f09a09b
dev-hannahk <dev.hannahk@gmail.com>
2023-07-08 19:17:02 +0900

    auth ui

--- PublicGalleryHannah/screens/SignInScreen.js
+++ PublicGalleryHannah/screens/SignInScreen.js
@@ -1,16 +1,129 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import BorderedInput from '../components/BorderedInput';
+import CustomButton from '../components/CustomButton';
 
-function SignInScreen() {
+function SignInScreen({navigation, route}) {
+  const {isSignUp} = route.params ?? {};
+
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const createChangeTextHandler = name => value => {
+    setForm({...form, [name]: value});
+  };
+
+  // const createChangeTextHandler = ({name, text}) => {
+  //   setForm({...form, [name]: text});
+  // };
+
+  const onSubmit = () => {
+    Keyboard.dismiss();
+    console.log({form});
+  };
+
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
   return (
-    <SafeAreaView style={styles.fullscreen}>
-      <Text style={styles.text}>PublicGallery</Text>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      style={styles.KeyboardAvoidingView}
+      behavior={Platform.select({ios: 'padding'})}>
+      <SafeAreaView style={styles.fullscreen}>
+        <Text style={styles.text}>PublicGallery</Text>
+        <View style={styles.form}>
+          <BorderedInput
+            hasMarginBottom
+            placeholder="이메일"
+            value={form.email}
+            // onChangeText={text => createChangeTextHandler('email')}
+            onChangeText={createChangeTextHandler('email')}
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoCompleteType="email"
+            keyboardType="email-address"
+            returnkeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <BorderedInput
+            placeholder="비밀번호"
+            hasMarginBottom={isSignUp}
+            value={form.password}
+            // onChangeText={text => createChangeTextHandler('password')}
+            ref={passwordRef}
+            onChangeText={createChangeTextHandler('password')}
+            secureTextEntry
+            returnkeyType={isSignUp ? 'next' : 'done'}
+            onSubmitEditing={() => {
+              if (isSignUp) {
+                confirmPasswordRef.current.focus();
+              } else {
+                onSubmit();
+              }
+            }}
+          />
+          {isSignUp && (
+            <BorderedInput
+              placeholder="비밀번호 확인"
+              value={form.confirmPassword}
+              // onChangeText={text => createChangeTextHandler('confirmPassword')}
+              ref={confirmPasswordRef}
+              onChangeText={createChangeTextHandler('confirmPassword')}
+              secureTextEntry
+              returnkeyType="done"
+              onSubmitEditing={onSubmit}
+            />
+          )}
+          <View style={styles.buttons}>
+            {isSignUp ? (
+              <>
+                <CustomButton
+                  title="회원가입"
+                  hasMarginBottom
+                  onPress={onSubmit}
+                />
+                <CustomButton
+                  title="로그인"
+                  theme="secondary"
+                  onPress={() => navigation.goBack()}
+                />
+              </>
+            ) : (
+              <>
+                <CustomButton
+                  title="로그인"
+                  hasMarginBottom
+                  onPress={onSubmit}
+                />
+                <CustomButton
+                  title="회원가입"
+                  theme="secondary"
+                  onPress={() => navigation.push('SignIn', {isSignUp: true})}
+                />
+              </>
+            )}
+          </View>
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  KeyboardAvoidingView: {
+    flex: 1,
+  },
   fullscreen: {
     flex: 1,
     alignItems: 'center',
@@ -19,6 +132,14 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 32,
     fontWeight: 'bold',
+  },
+  form: {
+    marginTop: 64,
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+  buttons: {
+    marginTop: 64,
   },
 });
 

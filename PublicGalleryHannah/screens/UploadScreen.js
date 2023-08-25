b10dc5d07eb13d3b6708fa9c0976c7ec4b8a872b
Heejin Kim <dev.hannahk@gmail.com>
2023-08-25 11:40:24 +0900

    upload post w/ file

--- PublicGalleryHannah/screens/UploadScreen.js
+++ PublicGalleryHannah/screens/UploadScreen.js
@@ -7,8 +7,14 @@ import {
   Animated,
   Keyboard,
   useWindowDimensions,
+  Platform,
+  KeyboardAvoidingView,
 } from 'react-native';
 import IconRightButton from '../components/IconRightButton';
+import storage from '@react-native-firebase/storage';
+import {useUserContext} from '../contexts/UserContext';
+import {v4} from 'uuid';
+import {createPost} from '../lib/post';
 
 function UploadScreen() {
   const route = useRoute();
@@ -18,9 +24,25 @@ function UploadScreen() {
   const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
   const [description, setDescription] = useState('');
   const navigation = useNavigation();
-  const onSubmit = useCallback(() => {
-    // TODO: 포스트 작성 로직 구현
-  }, []);
+  const {user} = useUserContext();
+
+  const onSubmit = useCallback(async () => {
+    navigation.pop();
+    const asset = res.assets[0];
+
+    const extension = asset.fileName.split('.').pop();
+    const reference = storage().ref(`/photo/${user.id}/${v4()}.${extension}`);
+    if (Platform.OS === 'android') {
+      await reference.putString(asset.base64, 'base64', {
+        contentType: asset.type,
+      });
+    } else {
+      await reference.putFile(asset.uri);
+    }
+
+    const photoURL = await reference.getDownloadURL();
+    await createPost({description, photoURL, user});
+  }, [res, user, description, navigation]);
 
   useEffect(() => {
     const didShow = Keyboard.addListener('keyboardDidShow', () =>
@@ -52,21 +74,28 @@ function UploadScreen() {
   }, [navigation, onSubmit]);
 
   return (
-    <View style={styles.block}>
-      <Animated.Image
-        source={{uri: res.assets[0]?.uri}}
-        style={[styles.image, {height: animation}]}
-        resizeMode="cover"
-      />
-      <TextInput
-        style={styles.input}
-        multiline={true}
-        placeholder="이 사진에 대한 설명을 입력하세요..."
-        textAlignVertical="top"
-        value={description}
-        onChangeText={setDescription}
-      />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.select({ios: 'height'})}
+      style={styles.block}
+      keyboardVerticalOffset={Platform.select({
+        ios: 180,
+      })}>
+      <View style={styles.block}>
+        <Animated.Image
+          source={{uri: res.assets[0]?.uri}}
+          style={[styles.image, {height: animation}]}
+          resizeMode="cover"
+        />
+        <TextInput
+          style={styles.input}
+          multiline={true}
+          placeholder="이 사진에 대한 설명을 입력하세요..."
+          textAlignVertical="top"
+          value={description}
+          onChangeText={setDescription}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
